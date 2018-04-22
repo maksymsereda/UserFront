2687c06983ea059f4e3e5c64d82d9750caa4d662
msereda206 <sereda206@gmail.com>
2018-04-22 17:04:09 +0200

    Dao, service folders added

--- src/main/java/com/userfront/controller/HomeController.java
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.userfront.controller;
 
 import com.userfront.domain.User;
+import com.userfront.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +14,9 @@ import java.util.Set;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping("/")
     public String home(){
@@ -32,21 +37,20 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public void signupPost(@ModelAttribute("user") User user, Model model){
-        /*if (userService.checkUserExists(user.getUserName(), user.getEmail())){
+    public String signupPost(@ModelAttribute("user") User user, Model model){
+        if (userService.checkUserExists(user.getUsername(), user.getEmail())){
             if (userService.checkEmailExists(user.getEmail())){
                 model.addAttribute("emailExists",true);
             }
-            if (userService.checkUsernameExists(user.getUserName())){
+            if (userService.checkUsernameExists(user.getUsername())){
                 model.addAttribute("usernameExists",true);
             }
             return "signup";
         }else {
-            Set<UserRole> userRoles = new HashSet<>();
-            userRoles.add(new UserRole(user, roleDao.findByName("USER")));
-            userService.createUser(user,userRoles);
+            
+            userService.save(user);
 
             return "redirect:/";
-        }*/
+        }
     }
 }
